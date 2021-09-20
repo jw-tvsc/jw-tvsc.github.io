e5a7e3c0ffd2c0417159b23c85b2606d70cf6689
Jason Ward <jw@tigersvscrime.com>
2021-09-20 16:37:00 -0600

    added text and favicon

--- low-gloss/src/App.vue
+++ low-gloss/src/App.vue
@@ -3,6 +3,7 @@
 </script>
 
 <template>
+<div id="header">LOW GLOSS <span id="vnum">v0.0.0</span></div>
  <video playsinline autoplay muted loop id="bgstatic">
    <source src="./assets/static1.mp4" type="video/mp4">
  </video>
@@ -19,7 +20,24 @@
   margin: 0;
   padding:0;
 }
+
+#header{
+  font-family: ocr-b-std, monospace;
+  font-weight: 400;
+  font-style: normal;
+  font-size: 3rem;
+  position: absolute;
+  padding: 2rem;
+  color: #000;
+  z-index: 1;
+}
+
+#vnum{
+  font-size: 1rem;
+}
+
 video{
+  z-index: 0;
   object-fit: cover;
   width: 100vw;
   height: 100vh;
